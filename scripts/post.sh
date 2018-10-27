4688e1d4eadefa76cb79f2d0948d38114c7e1bb7
M4rk9696 <nimalan.m@protonmail.com>
2018-10-28 01:19:21 +0530

    fix: fix post script, docs: update prev blog

--- scripts/post.sh
+++ scripts/post.sh
@@ -3,5 +3,5 @@ NAME=$DATE'-'$1
 cp $1 ../website/blog/$NAME
 rm $1
 cd ..
-git add ../website/blog/$1
+git add website/blog/$NAME
 git commit -m "blog: '$NAME'" --author="nimalan.m <nimalan.m@protonmail.com>"
